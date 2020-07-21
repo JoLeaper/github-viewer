37642bc22c22b01c9a5c8bc438c83dfa58da12d8
JoLeaper <joleap123@gmail.com>
2020-07-21 15:10:06 -0700

    passing in userName as props

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -5,20 +5,21 @@ import UserRepos from '../components/UserRepos/UserRepos';
 import { getUserInfo } from '../services/getUserInfo';
 
 export default class MainContainer extends Component {
-  // state = {
-
-  // }
+  state = {
+    userName: ''
+  }
   // componentDidMount() {
   //   getUserInfo('JoLeaper')
   //     .then(res => console.log(res));
   // }
 
   render() {
+    const { userName } = this.state;
     return (
       <div>
         <SearchBar />
-        <UserInfo />
-        <UserRepos />
+        <UserInfo userName={userName} />
+        <UserRepos userName={userName}/>
       </div>
     );
   }
